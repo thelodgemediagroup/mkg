b6e2fc0e6134a5d254dc34aa513903942ac81e9b
mkgdetroit <mkgdetroit@p3plcpnl0127.prod.phx3.secureserver.net>
2014-12-11 09:07:32 -0700

    add new assets

--- fuel/application/cache/dwoo/compiled/15c3a32294594cf5e36a3cf5eeebb0e7.d17.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ?><p>This is test data to display for a blog post.</p><?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
--- fuel/application/cache/dwoo/compiled/48bc6b402e3b048cc19e624f656b9191.d17.php
+++
@@ -1,5 +0,0 @@
-<?php
-/* template head */
-/* end template head */ ob_start(); /* template body */ ?><h1>This is the Homepage. Inserted into _layouts</h1><?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
