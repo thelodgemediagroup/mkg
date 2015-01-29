6d225779a5034701f3cb25537bf64413ef179e2e
scottdavidlyon <scottdavidlyon@gmail.com>
2015-01-28 22:32:17 -0500

    Removed references to Google Calendar.

--- fuel/application/views/_blocks/mkg_calendar.php
+++ fuel/application/views/_blocks/mkg_calendar.php
@@ -1,27 +1,63 @@
-<?php $CI->load->library('gcalendar/gcalendar'); ?>
-<?php $cal = $CI->gcalendar->get_calendar(); ?>
+<?php //$CI->load->library('gcalendar/gcalendar'); ?>
+<?php //$cal = $CI->gcalendar->get_calendar(); ?>
 
 		<article class="article-container">      
-			<div class="container" >
-			
-				<div class="row" >
-				  <div class="col-xs-12">
-				  <h2 class="headers">MKGD Schedule</h2>
-				  <span class="line" >
-					  <span class="sub-line" ></span>
-				  </span>
-				  </div>
-				</div>
-							
-				<div class="row" >
-					<div class="col-xs-12">
-						<div class="gce-page-grid">
-							<?php echo $cal; ?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<h2>MKGD 2015 Class Schedule</h2>
+						<p>MKG Detroit offers Pop-Up classes out of a rotating list of locations around the Detroit Metro area, in an effort to make our classes, and our community, available to people regardless of geography. Please register on our mailing list for information on joining or dropping in on a class.</p>
+						<p>MKG Detroit encourages you to try all the classes offered, as opposed to just specializing in one specific art.  All classes are specifically designed to help your development each area of these arts.  We do not take a singular approach.  This way, everyone achieves more.</p>
+						<p>Also, remember to follow us on <a href="https://www.facebook.com/MKGDetroit" target="_blank">Facebook</a> and <a href="https://twitter.com/MKGDetroit" target="_blank">Twitter</a> for real-time updates on classes, special events, and re-scheduling notices.</p>
+					
+						<div class="class">
+
+							<h3 class="classDay">Tuesday&nbsp;&nbsp;&nbsp;6:00-8:30pm</h3>
+
+							<div class="className">
+								6pm &mdash; <a href="/classes/filipino_kali_weaponry" title="Filipino Kali Weaponry">Filipino Kali Weaponry</a><br/>
+								7pm &mdash; <a href="/classes/phase_program" title="Phase One Mixed Arts">Phase One Mixed Arts</a><br/>
+							</div>
+
+							<div class="classTime">
+								Located at the <a href="https://www.google.com/maps/place/Brazilian+Jiu+Jitsu+Club+of+Michigan/@42.505165,-83.222726,15z/data=!4m2!3m1!1s0x0:0x44e1437c2052b50" title="Map to BJJ Club of Michigan" target="_blank">
+								BJJ Club of Michigan</a><br/>
+								29305 Southfield Rd, Southfield 
+							</div>
+
 						</div>
+
+						<div class="class">
+							<h3 class="classDay">Thursday&nbsp;&nbsp;&nbsp;6:00-8:30pm</h3>
+							<div class="className">
+								6pm &mdash; <a href="/classes/jeet_kune_do" title="Panantukan / Jeet Kune Do Concepts">Panantukan / Jeet Kune Do Concepts</a><br/>
+								7:15pm &mdash;  <a href="/classes/muay_thai_thai_boxing" title="MKG Muay Thai Kickboxing">Muay Thai Kickboxing</a><br/>
+							</div>
+							<div class="classTime">
+								Located at the <a href="https://www.google.com/maps/place/Brazilian+Jiu+Jitsu+Club+of+Michigan/@42.505165,-83.222726,15z/data=!4m2!3m1!1s0x0:0x44e1437c2052b50" title="Map to BJJ Club of Michigan" target="_blank">
+								BJJ Club of Michigan</a><br/>
+								29305 Southfield Rd, Southfield 
+							</div>
+						</div>
+
+						<div class="class">
+							<h3 class="classDay">Sunday&nbsp;&nbsp;&nbsp;12:00-3:00pm</h3>
+							<div class="className">
+								12pm &mdash; <a href="/classes/muay_thai_thai_boxing" title="MKG Muay Thai Kickboxing">MKG Muay Thai Kickboxing (Motown Muay Thai)</a><br/>
+								1pm &mdash;  <a href="/classes/filipino_kali_weaponry" title="Filipino Kali Weaponry">Filipino Kali Weaponry</a><br/>
+								2pm &mdash; <a href="/classes/jeet_kune_do" title="Panantukan / Jeet Kune Do Concepts">Panantukan / Jeet Kune Do Concepts</a><br/>
+							</div>
+							<div class="classTime">
+								Located at <a href="https://www.google.com/maps/place/True+Body+Fitness/@42.331653,-83.066299,15z/data=!4m2!3m1!1s0x0:0xa7d64500cf96c1ea" title="Map to True Body Fitness" target="_blank">True Body Fitness</a><br/>
+								1438 Michigan Ave, Detroit (Corktown) 
+							</div>
+						</div>
+
+						<br/><br/><br/><br/>
+
 					</div>
 				</div>
-			
-			</div>            
+			</div>      
 		</article>  
 		<!-- end Table -->
 	</section>
