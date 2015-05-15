2f3d40ce996d574204838d23af88890f6699c7b9
Scott Lyon <scottl@lpt-mbp401.local>
2015-05-15 19:56:14 -0400

    Updated schedule.

--- fuel/application/views/schedule.php
+++ fuel/application/views/schedule.php
@@ -14,7 +14,7 @@
 								<li><h6><a href="<?php echo base_url(); ?>">Home</a></h6></li>
 								<li><i class="separator entypo-play" ></i></li>
 								<li><h6>Schedule</h6></li>
-							</ul>                    
+							</ul>
 						</li>
 					</ul>
 				</div>
@@ -31,36 +31,28 @@
 						<p>MKG Detroit offers Pop-Up classes out of a rotating list of locations around the Detroit Metro area, in an effort to make our classes, and our community, available to people regardless of geography. Please register on our mailing list for information on joining or dropping in on a class.</p>
 						<p>MKG Detroit encourages you to try all the classes offered, as opposed to just specializing in one specific art.  All classes are specifically designed to help your development each area of these arts.  We do not take a singular approach.  This way, everyone achieves more.</p>
 						<p>Also, remember to follow us on <a href="https://www.facebook.com/MKGDetroit" target="_blank">Facebook</a> and <a href="https://twitter.com/MKGDetroit" target="_blank">Twitter</a> for real-time updates on classes, special events, and re-scheduling notices.</p>
-					
+
 						<div class="class">
 
 							<h3 class="classDay">Tuesday&nbsp;&nbsp;&nbsp;6:00-8:30pm</h3>
 
 							<div class="className">
-								6pm &mdash; <a href="/classes/filipino_kali_weaponry" title="Filipino Kali Weaponry">Filipino Kali Weaponry</a><br/>
-								7pm &mdash; <a href="/classes/phase_program" title="Phase One Mixed Arts">Phase One Mixed Arts</a><br/>
+								PHASE ONE NIGHT!
 							</div>
 
-							<div class="classTime">
-								Located at the <a href="https://www.google.com/maps/place/Brazilian+Jiu+Jitsu+Club+of+Michigan/@42.505165,-83.222726,15z/data=!4m2!3m1!1s0x0:0x44e1437c2052b50" title="Map to BJJ Club of Michigan" target="_blank">
-								BJJ Club of Michigan</a><br/>
-								29305 Southfield Rd, Southfield 
-							</div>
-
-						</div>
-
-						<div class="class">
-							<h3 class="classDay">Thursday&nbsp;&nbsp;&nbsp;6:00-8:30pm</h3>
 							<div class="className">
-								6pm &mdash; <a href="/classes/jeet_kune_do" title="Panantukan / Jeet Kune Do Concepts">Panantukan / Jeet Kune Do Concepts</a><br/>
-								7:15pm &mdash;  <a href="/classes/muay_thai_thai_boxing" title="MKG Muay Thai Kickboxing">Muay Thai Kickboxing</a><br/>
+								6pm &mdash; <a href="/classes/filipino_kali_weaponry" title="Weaponry Fundamentals &amp; Mobility">Weaponry Fundamentals &amp; Mobility</a><br/>
+								7:15pm &mdash; <a href="/classes/phase_program" title="Mixed Kickboxing and Conditioning">Mixed Kickboxing and Conditioning</a><br/>
 							</div>
+
 							<div class="classTime">
 								Located at the <a href="https://www.google.com/maps/place/Brazilian+Jiu+Jitsu+Club+of+Michigan/@42.505165,-83.222726,15z/data=!4m2!3m1!1s0x0:0x44e1437c2052b50" title="Map to BJJ Club of Michigan" target="_blank">
 								BJJ Club of Michigan</a><br/>
-								29305 Southfield Rd, Southfield 
+								29305 Southfield Rd, Southfield
 							</div>
+
 						</div>
+
 
 						<div class="class">
 							<h3 class="classDay">Sunday&nbsp;&nbsp;&nbsp;12:00-3:00pm</h3>
@@ -71,7 +63,7 @@
 							</div>
 							<div class="classTime">
 								Located at <a href="https://www.google.com/maps/place/True+Body+Fitness/@42.331653,-83.066299,15z/data=!4m2!3m1!1s0x0:0xa7d64500cf96c1ea" title="Map to True Body Fitness" target="_blank">True Body Fitness</a><br/>
-								1438 Michigan Ave, Detroit (Corktown) 
+								1438 Michigan Ave, Detroit (Corktown)
 							</div>
 						</div>
 
@@ -82,18 +74,18 @@
 		<!-- end Intro -->
 
 		<!-- Table -->
-		<article class="article-container">      
+		<article class="article-container">
 			<div class="container" >
-			
+
 				<div class="row" >
 				  <div class="col-xs-12">
-				  
+
 				  <span class="line" >
 					  <span class="sub-line" ></span>
 				  </span>
 				  </div>
 				</div>
-							
+
 				<div class="row" >
 					<div class="col-xs-12">
 						<div class="gce-page-grid">
@@ -101,7 +93,7 @@
 						</div>
 					</div>
 				</div>
-			
+
 			</div>
 		</article>
 		<!-- end Table -->
